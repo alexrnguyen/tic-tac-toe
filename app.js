538b5d22b4fda15e4ec347f5bafc72fe8b105c0d
alexrnguyen <alexander.nguyen@shaw.ca>
2023-08-02 19:10:52 -0600

    Prevent squares from changing after the game ends

--- app.js
+++ app.js
@@ -10,39 +10,31 @@ const playerFactory = (name, marker) => {
 
 // Stores every move made by both players
 const gameboard = (() => {
-  const boardGrid = document.querySelector(".gameboard"); // Move to displayController??? ---------------------------------------------------------------------------
-  let board = [];
+  let _board = [];
   const createBoard = () => {
     for (let i = 0; i < 9; i++) {
       const square = squareFactory(i, "");
-      board.push(square);
+      _board.push(square);
     }
-    console.log(board);
   };
 
-  const placeMarker = (index, marker) => {
-    // Place marker on gameboard (displayController ensures the move is valid before passing the move to this function)
-    console.log("Marker placed");
-    board[index].marker = marker;
+  // Set square on gameboard to given marker
+  const setSquare = (index, marker) => {
+    _board[index].marker = marker;
   };
 
-  const threeInARow = () => {
-    return false;
-  };
-
-  const allSquaresFilled = () => {
-    return board.every((square) => square.marker !== "");
+  const getBoard = () => {
+    return _board;
   };
 
   const resetBoard = () => {
-    board = [];
+    _board = [];
   };
 
   return {
     createBoard,
-    placeMarker,
-    threeInARow,
-    allSquaresFilled,
+    setSquare,
+    getBoard,
     resetBoard,
   };
 })();
@@ -51,53 +43,97 @@ const gameboard = (() => {
 const displayController = (() => {
   const player1 = playerFactory("Player 1", "X");
   const player2 = playerFactory("Player 2", "O");
+  let currentPlayer = player1;
   const restartButton = document.querySelector(".restart-button");
   const squares = document.querySelectorAll(".square");
 
   const startGame = () => {
     gameboard.createBoard();
-    playGame();
+    currentPlayer = player1;
+    updateDisplay(currentPlayer);
   };
 
-  const playGame = () => {
-    let currentPlayer = player1;
-    squares.forEach((square) => {
-      square.addEventListener("click", () => {
-        playTurn(currentPlayer, square);
-        if (checkIfGameOver()) {
-          reportWinner();
-        }
-        if (currentPlayer === player1) {
-          currentPlayer = player2;
-        } else {
-          currentPlayer = player1;
-        }
-      });
-    });
+  const handleClick = (square) => {
+    playTurn(currentPlayer, square);
+    checkIfGameOver();
+    if (currentPlayer === player1) {
+      currentPlayer = player2;
+    } else {
+      currentPlayer = player1;
+    }
+    updateDisplay(currentPlayer);
   };
 
   const playTurn = (player, selectedSquare) => {
-    // Players can only choose empty squares
-    if (selectedSquare.textContent === "") {
-      gameboard.placeMarker(selectedSquare.dataset.index, player.marker);
+    // Players can only choose empty squares while the game is still being played (no winner)
+    const winningPlayer = checkForWinner();
+    if (selectedSquare.textContent === "" && winningPlayer === null) {
+      gameboard.setSquare(selectedSquare.dataset.index, player.marker);
       selectedSquare.textContent = player.marker;
     }
   };
 
+  const updateDisplay = (currentPlayer) => {
+    const playerTurnDisplay = document.querySelector(".player-turn");
+    playerTurnDisplay.textContent = `${currentPlayer.name}\'s Turn`;
+  };
+
   const checkIfGameOver = () => {
-    if (gameboard.threeInARow()) {
-      return true;
-    } else if (gameboard.allSquaresFilled()) {
-      return true;
-    } else {
-      return false;
+    const winningPlayer = checkForWinner();
+    if (winningPlayer !== null) {
+      reportWinner(winningPlayer);
+    } else if (checkForTie()) {
+      reportTie();
     }
   };
 
-  const reportWinner = () => {
-    console.log("Game Over!");
-    squares.forEach((square) => square.removeEventListener);
+  const checkForWinner = () => {
+    const winConditions = [
+      [0, 3, 6], //Columns
+      [1, 4, 7],
+      [2, 5, 8],
+      [0, 1, 2], //Rows
+      [3, 4, 5],
+      [6, 7, 8],
+      [0, 4, 8], //Diagonals
+      [2, 4, 6],
+    ];
+
+    for (condition of winConditions) {
+      const player1Wins = gameboard
+        .getBoard()
+        .filter((square) => condition.includes(square.index))
+        .every((square) => square.marker === player1.marker);
+      const player2Wins = gameboard
+        .getBoard()
+        .filter((square) => condition.includes(square.index))
+        .every((square) => square.marker === player2.marker);
+
+      if (player1Wins) {
+        return player1;
+      } else if (player2Wins) {
+        return player2;
+      }
+    }
+    return null;
   };
+
+  const checkForTie = () => {
+    return gameboard.getBoard().every((square) => square.marker !== "");
+  };
+
+  const reportWinner = (winningPlayer) => {
+    console.log(`${winningPlayer.name} Wins!`);
+  };
+
+  const reportTie = () => {
+    console.log("It's a Tie!");
+  };
+
+  // Event listeners
+  squares.forEach((square) => {
+    square.addEventListener("click", () => handleClick(square));
+  });
 
   restartButton.addEventListener("click", () => {
     gameboard.resetBoard();
@@ -106,8 +142,6 @@ const displayController = (() => {
   });
   return {
     startGame,
-    playGame,
-    // ...
   };
 })();
 
